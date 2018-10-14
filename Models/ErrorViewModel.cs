4f4c1cf88487af46ce0669f3686906e90a8e347a
jamill <jesus_reyes@usmp.pe>
2018-10-13 20:11:06 -0500

    sub

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jbchorg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
