4a69747c722d7f248b6c6a85da717f47deaa7a3d
rosmemoli <rosmenanami@gmail.com>
2018-10-27 03:48:48 -0500

    rpmg

--- Models/Donacion.cs
+++ Models/Donacion.cs
@@ -3,6 +3,17 @@ namespace jbchorg.Models
 {
     public class Donacion
     {
-        
+        public int Id {get; set;}
+        [Required(ErrorMessage = "Ingrese su Nombre")]
+        public string Nombre { get; set; }
+        public string APaterno { get; set; }
+        public string AMaterno { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "Ingrese su Email")]
+        public string Correo { get; set; }
+
+        public string TipoAsociado { get; set; }
+        public string GAcademico  { get; set; }
+        public string Area { get; set; }
     }
 }
