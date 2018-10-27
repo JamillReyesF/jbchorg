b03b3270b327879b0e3c0ab591849e7d9de94980
rosmemoli <rosmenanami@gmail.com>
2018-10-27 03:31:11 -0500

    rpmg

--- Controllers/DonacionController.cs
+++ Controllers/DonacionController.cs
@@ -1,7 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using jbchorg.Models;
+using System.Net.Mail;
+
 namespace jbchorg.Controllers
 {
-    public class DonacionController
+    public class DonacionController : Controller
     {
+        private readonly JbchorgDBContext context;
+        public DonacionController(JbchorgDBContext c){
+            context = c;
+        }
+        public IActionResult Donacion()
+        {
+            ViewData["Message"] = "Your contact page.";
         
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Donacion(Donacion don)
+        {
+            if(ModelState.IsValid){
+                context.Add(don);    
+                context.SaveChanges();
+               return RedirectToAction("DConfirmacion");
+            }
+             return View(don);
+        }
+         public IActionResult DConfirmacion()
+        {
+            ViewData["Message"] = "Your contact page.";
+        
+            return View();
+        }
     }
 }
