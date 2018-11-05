2da19783a7d3c7d28c51585e7d88a628f763db6c
rosmemoli <rosmenanami@gmail.com>
2018-11-04 23:37:55 -0500

    rpmg

--- Models/Donacion.cs
+++ Models/Donacion.cs
@@ -12,11 +12,19 @@ namespace jbchorg.Models
         public string Correo { get; set; } 
         [Required(ErrorMessage = "Ingrese su Nombre")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Ingrese su Apellido Paterno")]
         public string APaterno  { get; set; }
+        [Required(ErrorMessage = "Ingrese su Apellido Materno")]
         public string AMaterno { get; set; }
+        [Required(ErrorMessage = "Ingrese su DNI")]
+        public string DNI { get; set; }
+        [Required(ErrorMessage = "Ingrese su Movil")]
         public string Movil { get; set; }
+        [Required(ErrorMessage = "Ingrese el Monto")]
         public string Monto { get; set; }
+        [Required(ErrorMessage = "Ingrese el Banco")]
         public string Banco  { get; set; }
+        [Required(ErrorMessage = "Ingrese su Número de Tarjeta")]
         public string Tarjeta { get; set; }
     }
 }
